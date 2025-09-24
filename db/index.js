8e064aaced71af85c88a4aacdde2b367fa1dc412
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-09-24 22:14:59 +0530

    first commit: made a skeleton

---
+++ db/index.js
@@ -0,0 +1,15 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+//loads all the .env variables in process.env
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+export default pool;
